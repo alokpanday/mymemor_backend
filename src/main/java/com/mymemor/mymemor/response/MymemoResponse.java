473092587188552107ad02f1fa5e946190a6130a
alokpanday <36020903+alokpanday@users.noreply.github.com>
2020-01-12 13:01:09 +0530

    Update MymemoResponse.java

--- src/main/java/com/mymemor/mymemor/response/MymemoResponse.java
+++ src/main/java/com/mymemor/mymemor/response/MymemoResponse.java
@@ -1,5 +1,21 @@
 package com.mymemor.mymemor.response;
 
+import java.util.List;
+
+import com.mymemor.mymemor.model.Memory;
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class MymemoResponse {
+	@Getter
+	@Setter
+	String status;
+	@Getter
+	@Setter
+	String error;
+	@Getter
+	@Setter
+	List<Memory> memories ;
 
 }
